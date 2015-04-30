c5115925f5782cdca8850b2e8e1cb5cdbf603480
wuxingyu1983 <wuxingyu@wuxingyudeMacBook-Pro.local>
2015-05-01 00:44:26 +0800

    解决了 puzzle

--- birthday.pl
+++ birthday.pl
@@ -26,9 +26,41 @@ have_same_day(Day, [Head|Tail], Birthdays) :- Head = (X, Day1),
 												(Day1 = Day2) -> have_same_day(Day, Tail, BirthdaysTail), append(BirthdaysTail, [Head], Birthdays);
 															 have_same_day(Day, Tail, BirthdaysTail), append(BirthdaysTail, [], Birthdays).
 
+funcA([]).
+funcA([Head|Tail]) :-	birthdays(List),
+						have_same_day(Head, List, SameDay),
+						length(SameDay, LengthD),
+						LengthD > 1,
+						funcA(Tail).
 
-reverse_list([],[]).
-reverse_list(List,ReverseList) :- List = [Head|Tail],
-         reverse_list(Tail, ReverseTail),
-		          append(ReverseTail,[Head],ReverseList).
+statement3(X) :- 	birthday(X),
+				    birthdays(List),
+					have_same_month(X, List, SameMonth),
+					length(SameMonth, LengthM),
+					LengthM > 1,
+					funcA(SameMonth).
+
+list3([], []).
+list3([Head|Tail], X) :-	statement3(Head) -> list3(Tail, XTail), append(XTail, [Head], X);
+										list3(Tail, XTail), append(XTail, [], X).
+
+statement4(X) :-	birthday(X),
+				    birthdays(List),
+					list3(List, List1),
+					have_same_day(X, List1, SameDay),
+					length(SameDay, LengthD),
+					LengthD = 1.
+
+list4([], []).
+list4([Head|Tail], X) :-	(statement3(Head), statement4(Head)) -> list4(Tail, XTail), append(XTail, [Head], X);
+										list4(Tail, XTail), append(XTail, [], X).
+
+
+cheryl(X) :-	statement3(X),
+				statement4(X),
+				birthdays(List),
+				list4(List, List2),
+				have_same_month(X, List2, SameMonth),
+				length(SameMonth, LengthM),
+				LengthM = 1.
 
